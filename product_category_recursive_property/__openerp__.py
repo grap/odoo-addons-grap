1bd8c7e8204dc4bd30681c5c89e85f518735275e
Sylvain LE GAL <sylvain.legal@grap.coop>
2014-10-29 17:59:24 +0100

    [REF] 'product_category_recursive_property' flake8 and licensing;

--- product_category_recursive_property/__openerp__.py
+++ product_category_recursive_property/__openerp__.py
@@ -1,29 +1,47 @@
 # -*- encoding: utf-8 -*-
-################################################################################
-#    See Copyright and Licence Informations undermentioned.
-################################################################################
+##############################################################################
+#
+#    Product Category - Recursive property for Odoo
+#    Copyright (C) 2013 GRAP (http://www.grap.coop)
+#    @author Sylvain LE GAL (https://twitter.com/legalsylvain)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
 
 {
     'name': 'Product Category - Recursive property',
     'version': '0.1',
+    'summary': "Propagate recursively properties for product category",
     'category': 'Product',
     'description': """
 Propagate recursively properties for product category
 =====================================================
 Features :
 ----------
-    * When an user set an accounting property to a product category, the parameter will be set to all child.
+    * When an user set an accounting property to a product category, the
+    parameter will be set to all child.
 
 Copyright and Licence :
 -----------------------
-    * 2014, Groupement Régional Alimentaire de Proximité (http://www.grap.coop/)
+    * 2013, Groupement Régional Alimentaire de Proximité (http://www.grap.coop/)
     * Licence : AGPL-3 (http://www.gnu.org/licenses/)
 
-Contacts :
-----------
-    * Sylvain LE GAL (https://twitter.com/legalsylvain) ;
-    * <informatique@grap.coop> for any help or question about this module.
-    """,
+Copyright, Authors and Licence:
+-------------------------------
+    * Copyright: 2013, GRAP: Groupement Régional Alimentaire de Proximité;
+    * Author: Sylvain LE GAL (https://twitter.com/legalsylvain);""",
     'author': 'GRAP',
     'website': 'http://www.grap.coop',
     'license': 'AGPL-3',
@@ -31,9 +49,4 @@ Contacts :
         'product',
         'account',
     ],
-    'init_xml': [],
-    'demo_xml': [],
-    'update_xml': [],
-    'installable': True,
-    'application': True,
 }
--- product_category_recursive_property/model/product_category.py
+++ product_category_recursive_property/model/product_category.py
@@ -1,13 +1,31 @@
 # -*- encoding: utf-8 -*-
-################################################################################
-#    See __openerp__.py file for Copyright and Licence Informations.
-################################################################################
+##############################################################################
+#
+#    Product Category - Recursive property for Odoo
+#    Copyright (C) 2013 GRAP (http://www.grap.coop)
+#    @author Sylvain LE GAL (https://twitter.com/legalsylvain)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
 
 from openerp.osv.orm import Model
 
+
 class product_category(Model):
     _inherit = 'product.category'
-    
+
     _PRODUCT_CATEGORY_PROPERTY_LIST = [
         'property_stock_journal',
         'property_stock_account_input_categ',
@@ -17,24 +35,28 @@ class product_category(Model):
         'property_account_expense_categ',
     ]
 
-    ### Custom Function
+    # Custom Function
     def _propagate_properties_to_childs(self, cr, uid, ids, vals, context=None):
         values = {}
         for property_name in self._PRODUCT_CATEGORY_PROPERTY_LIST:
             if property_name in vals.keys():
                 values[property_name] = vals[property_name]
-        if values: 
+        if values:
             for pc in self.browse(cr, uid, ids, context=context):
                 child_ids = [x.id for x in pc.child_id]
                 self.write(cr, uid, child_ids, values, context=context)
 
-#    ### Overwrite Section
+    # Overload Section
     def create(self, cr, uid, vals, context=None):
-        category_id = super(product_category, self).create(cr, uid, vals, context=context)
-        self._propagate_properties_to_childs(cr, uid, [category_id], vals, context=context)
+        category_id = super(product_category, self).create(
+            cr, uid, vals, context=context)
+        self._propagate_properties_to_childs(
+            cr, uid, [category_id], vals, context=context)
         return category_id
 
     def write(self, cr, uid, ids, vals, context=None):
-        res = super(product_category, self).write(cr, uid, ids, vals, context=context)
-        self._propagate_properties_to_childs(cr, uid, ids, vals, context=context)
+        res = super(product_category, self).write(
+            cr, uid, ids, vals, context=context)
+        self._propagate_properties_to_childs(
+            cr, uid, ids, vals, context=context)
         return res
