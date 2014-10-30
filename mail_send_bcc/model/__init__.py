eecfe0f838133008f1b7fcfcae06a854680ce492
Sylvain LE GAL <sylvain.legal@grap.coop>
2014-10-30 17:02:54 +0100

    [REF] 'mail_send_bcc' move from 'odoo-addons-grap' to 'odoo-addons-misc'

--- mail_send_bcc/model/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- encoding: utf-8 -*-
-################################################################################
-#    See __openerp__.py file for Copyright and Licence Informations.
-################################################################################
-
-from . import ir_mail_server
-from . import res_users
