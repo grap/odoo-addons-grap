5cdf7577b41f52ed7d5963ec75d258dfde17ac77
Sylvain LE GAL <sylvain.legal@grap.coop>
2014-11-07 19:15:40 +0100

    [REF] 'pos_payment_list' flake8;

--- pos_payment_method_list/__openerp__.py
+++ pos_payment_method_list/__openerp__.py
@@ -1,26 +1,48 @@
-# -*- coding: utf-8 -*-
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Point Of Sale - Payment Methods List Module for Odoo
+#    Copyright (C) 2013-Today GRAP (http://www.grap.coop)
+#    @author Julien WESTE
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
 
 {
-    'name': 'GRAP - POS Payment Methods List',
-    'version': '2.X',
+    'name': 'Point Of Sale - Payment Methods List',
+    'version': '2.4',
     'category': 'Point of Sale',
     'description': """
-In the pos.payment wizard, display only the payment methods defined in the pos.config.
-Avoid conflicts with the other account.journal.search() calls
-======================================================================================================
+    * In the pos.payment wizard, display only the payment methods defined in
+      the pos.config.
+    * Avoid conflicts with the other account.journal.search() calls
+
+Copyright, Author and Licence :
+-------------------------------
+    * Copyright : 2014, Groupement Régional Alimentaire de Proximité;
+    * Author :
+        * Sylvain LE GAL (https://twitter.com/legalsylvain);
+    * Licence : AGPL-3 (http://www.gnu.org/licenses/)
     """,
-    'author': 'Sylvain LE GAL / Julien WESTE - Groupement Régional Alimentaire de Proximité (GRAP)',
+    'author': 'GRAP',
     'website': 'http://www.grap.coop',
     'license': 'AGPL-3',
     'depends': [
         'point_of_sale',
     ],
-    'init_xml': [],
-    'demo_xml': [],
-    'update_xml': [
-                'point_of_sale_view.xml',
-        ],
-    'installable': True,
-    'application': True,
-    'complexity': "easy",
+    'data': [
+        'point_of_sale_view.xml',
+    ],
 }
