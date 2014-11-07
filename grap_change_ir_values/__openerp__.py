f3b1a34392c142bf9117e80157e771572aae0a69
Sylvain LE GAL <sylvain.legal@grap.coop>
2014-11-07 21:44:59 +0100

    [REF] 'grap_change_ir_values' flake8

--- grap_change_ir_values/__openerp__.py
+++ grap_change_ir_values/__openerp__.py
@@ -1,7 +1,28 @@
-# -*- coding: utf-8 -*-
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    GRAP - Change Ir Values Module for Odoo
+#    Copyright (C) 2013-Today GRAP (http://www.grap.coop)
+#    @author Julien WESTE
+#    @author Sylvain LE GAL (https://twitter.com/legalsylvain)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
 
 {
-    'name': 'GRAP - Change some ir.values',
+    'name': 'GRAP - Change Ir Values',
     'version': '0.1',
     'category': 'GRAP - Custom',
     'description': """
@@ -9,28 +30,22 @@ Set ir.values for default feature
 =================================
 Features :
 ----------
-    * Model 'res.partner' ; field : 'notification_email_send' ; value : 'comment'.
+    * Model 'res.partner' ;
+      field : 'notification_email_send' ;
+      value : 'comment'.
 
-Contacts :
-----------
-    * Julien WESTE ;
-    * Sylvain LE GAL (https://twitter.com/legalsylvain) ;
-    * <informatique@grap.coop> for any help or question about this module.
+Copyright, Author and Licence :
+-------------------------------
+    * Copyright : 2014, Groupement Régional Alimentaire de Proximité;
+    * Author :
+        * Julien WESTE;
+        * Sylvain LE GAL (https://twitter.com/legalsylvain);
+    * Licence : AGPL-3 (http://www.gnu.org/licenses/)
     """,
-    'author': 'GRAP',
-    'website': 'http://www.grap.coop',
-    'license': 'AGPL-3',
     'depends': [
         'mail',
-        ],
-    'init_xml': [],
-    'update_xml': [
+    ],
+    'data': [
         'data/ir_values.yml',
-        ],
-    'demo_xml': [],
-     'css': [],
-    'application': True,
-    'complexity': "easy",
-    'installable': True,
-    'auto_install': False,
+    ],
 }
