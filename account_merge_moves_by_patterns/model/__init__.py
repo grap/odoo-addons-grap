cb8a5d6902abcb6a01b2c047d323e7a6eee9ba94
Sylvain LE GAL <sylvain.legal@grap.coop>
2014-11-07 01:16:31 +0100

    [REF] move 'account_merge_moves_by_patterns' from odoo-addons-grap to odoo-addons-misc

--- account_merge_moves_by_patterns/model/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- encoding: utf-8 -*-
-################################################################################
-#    See __openerp__.py file for Copyright and Licence Informations.
-################################################################################
-
-import account_merge_pattern
-import account_move
