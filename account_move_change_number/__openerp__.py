7dce995e57f500ef4df926757cc4c99d7d180945
Sylvain LE GAL <sylvain.legal@grap.coop>
2014-11-07 22:09:27 +0100

    [REF] 'account_move_change_number' flake8

--- account_move_change_number/__openerp__.py
+++ account_move_change_number/__openerp__.py
@@ -1,24 +1,49 @@
-# -*- coding: utf-8 -*-
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Account - Move Change Number Module for Odoo
+#    Copyright (C) 2013-Today GRAP (http://www.grap.coop)
+#    @author Sylvain LE GAL (https://twitter.com/legalsylvain)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
 {
-    'name': 'Change number of account move',
-    'version': '0.1',
+    'name': 'Account - Move Change Number',
+    'version': '1.0',
     'category': 'Accounting',
     'description': """
 Allow special user to rename account move
 =========================================
+
 Features :
 ----------
-    * Allow special user to rename account move ; Usefull if the account move doesn't have the good numeration. (for exemple if you change account move sequence) ; 
+    * Allow special user to rename account move ;
+      Usefull if the account move doesn't have the good numeration.
+      (for exemple if you change account move sequence) ;
 
 Technical informations :
 ------------------------
-    * change account move name with next sequence ; 
-    * add in the field 'narration' the old name ; 
+    * change account move name with next sequence ;
+    * add in the field 'narration' the old name ;
 
-Contacts :
-----------
-    * Sylvain LE GAL (https://twitter.com/legalsylvain); 
-    * <informatique@grap.coop> for any help or question about this module.
+Copyright, Author and Licence :
+-------------------------------
+    * Copyright : 2014, Groupement Régional Alimentaire de Proximité;
+    * Author :
+        * Sylvain LE GAL (https://twitter.com/legalsylvain);
+    * Licence : AGPL-3 (http://www.gnu.org/licenses/)
     """,
     'author': 'GRAP',
     'website': 'http://www.grap.coop',
@@ -26,14 +51,9 @@ Contacts :
     'depends': [
         'account',
     ],
-    'init_xml': [],
-    'demo_xml': [],
-    'update_xml': [
+    'data': [
         'security/res_groups_data.yml',
         'view/view.xml',
         'view/action.xml',
     ],
-    'application' : True,
-    'installable': True,
-    'auto_install': False,
 }
--- account_move_change_number/model/account_move.py
+++ account_move_change_number/model/account_move.py
@@ -1,12 +1,33 @@
-# -*- coding: utf-8 -*-
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Account - Move Change Number Module for Odoo
+#    Copyright (C) 2013-Today GRAP (http://www.grap.coop)
+#    @author Sylvain LE GAL (https://twitter.com/legalsylvain)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
 
 from openerp.osv.orm import Model
 from openerp.tools.translate import _
 
+
 class account_move(Model):
     _inherit = 'account.move'
-    
-    def rename_account_move_change_number(self, cr, uid, ids, context = None):
+
+    def rename_account_move_change_number(self, cr, uid, ids, context=None):
         for id in ids:
             old_account_move = self.browse(cr, uid, id, context=context)
             old_name = old_account_move.name
@@ -15,15 +36,16 @@ class account_move(Model):
             self.button_cancel(cr, uid, [id], context=context)
 
             # set name to "/"
-            self.write(cr, uid, [id], {'name': '/'}, context = context)
-            
+            self.write(cr, uid, [id], {'name': '/'}, context=context)
+
             # post account move
             self.post(cr, uid, [id], context=context)
             new_name = self.browse(cr, uid, id, context=context).name
-            
+
             # Add description of the change
             self.write(cr, uid, [id], {
-                'narration': old_narration + _("\nAccount move renamed old name : %s ; new name : %s" ) % (old_name,new_name,)
-                }, context = context)
+                'narration': old_narration + _(
+                    """\nAccount move renamed old name : %s ;"""
+                    """ new name : %s""") % (old_name, new_name)
+            }, context=context)
         return True
-    
