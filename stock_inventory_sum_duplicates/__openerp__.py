a69c770fc622c9bac855e33cde25f56c0847fa69
Sylvain LE GAL <sylvain.legal@grap.coop>
2014-11-07 21:14:04 +0100

    [REF] 'stock_inventory_sum_duplicates' flake8

--- stock_inventory_sum_duplicates/__openerp__.py
+++ stock_inventory_sum_duplicates/__openerp__.py
@@ -1,22 +1,45 @@
-# -*- coding: utf-8 -*-
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Stock Inventory - Sum Duplicates for Odoo
+#    Copyright (C) 2013-Today GRAP (http://www.grap.coop)
+#    @author Julien WESTE
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
 
 {
-    'name': 'Stock Inventory Sum Duplicates ',
-    'version': '1.0',
+    'name': 'Stock Inventory - Sum Duplicates',
+    'version': '1.1',
     'category': 'Stock',
     'description': """
-If a stock.inventory has several lines with same product, lot and location, the lines will be merged and the quantities will be summed before creating the stock.moves
+If a stock.inventory has several lines with same product, lot and location,
+the lines will be merged and the quantities will be summed before creating
+the stock.moves
+
+Copyright, Author and Licence :
+-------------------------------
+    * Copyright : 2013, Groupement Régional Alimentaire de Proximité;
+    * Author :
+        * Julien WESTE;
+    * Licence : AGPL-3 (http://www.gnu.org/licenses/)
     """,
-    'author': 'Sylvain LE GAL / Julien WESTE - Groupement Régional Alimentaire de Proximité (GRAP)',
+    'author': 'GRAP',
     'website': 'http://www.grap.coop',
     'license': 'AGPL-3',
     'depends': [
         'stock',
     ],
-    'init_xml': [],
-    'demo_xml': [],
-    'update_xml': [
-    ],
-    'installable': True,
-    'application': True,
 }
