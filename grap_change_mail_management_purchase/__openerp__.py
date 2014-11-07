f472f7933f79595633ea0063f8b162b794ff1c19
Sylvain LE GAL <sylvain.legal@grap.coop>
2014-11-07 19:18:07 +0100

    [REF] 'grap_change_mail_management_purchase' flake8;

--- grap_change_mail_management_purchase/__openerp__.py
+++ grap_change_mail_management_purchase/__openerp__.py
@@ -1,27 +1,47 @@
-# -*- coding: utf-8 -*-
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Account - Delete Null Account Move Module for Odoo
+#    Copyright (C) 2013-Today GRAP (http://www.grap.coop)
+#    @author Sylvain LE GAL (https://twitter.com/legalsylvain)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
 
 {
-    'name' : 'GRAP - Change mail management for purchases',
-    'version' : '2.0',
-    'category' : 'GRAP - Custom',
+    'name': 'GRAP - Change mail management for purchases',
+    'version': '2.0',
+    'category': 'GRAP - Custom',
     'description': """
-Change view of purchase
-=======================
+Change workflow of Mail in Purchase
+===================================
 
+Copyright, Author and Licence:
+-------------------------------
+    * Copyright: 2014, Groupement Régional Alimentaire de Proximité;
+    * Author:
+        * Sylvain LE GAL (https://twitter.com/legalsylvain);
+    * Licence: AGPL-3 (http://www.gnu.org/licenses/)
     """,
-    'author' : 'Sylvain LE GAL / Julien WESTE - Groupement Régional Alimentaire de Proximité (GRAP)',
+    'author': 'GRAP',
     'website': 'http://www.grap.coop',
     'license': 'AGPL-3',
-    'depends' : [
+    'depends': [
         'purchase',
     ],
-    'init_xml' : [],
-    'update_xml' : [
+    'data': [
         'purchase_view.xml',
     ],
-    'demo_xml': [],
-    'active': False,
-    'installable': True, 
-    'application' : True,
-    'complexity': "easy",
 }
