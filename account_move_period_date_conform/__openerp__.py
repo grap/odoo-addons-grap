71625aab169c6958fe09a1b4617732283751affa
Sylvain LE GAL <sylvain.legal@grap.coop>
2014-11-07 19:38:27 +0100

    [REF] 'account_move_period_date_conform' flake8

--- account_move_period_date_conform/__openerp__.py
+++ account_move_period_date_conform/__openerp__.py
@@ -1,27 +1,53 @@
 # -*- encoding: utf-8 -*-
-################################################################################
-#    See Copyright and Licence Informations undermentioned.
-################################################################################
+##############################################################################
+#
+#    Account Move - Date and Ceriod Conform Module for Odoo
+#    Copyright (C) 2013-Today GRAP (http://www.grap.coop)
+#    @author Sylvain LE GAL (https://twitter.com/legalsylvain)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
 {
-    'name': 'Account Move - Date and period conform',
-    'version': '0.1',
+    'name': 'Account Move - Date and Period Conform',
+    'version': '0.2',
     'category': 'Account',
     'description': """
-Prevent to have an account move with an invalid period and correct existing moves
-=================================================================================
+Prevent to have an account move with an invalid period and correct
+existing moves
+==================================================================
+==============
+
 Features :
 ----------
-    * Help accounting people by filling period_id correctly when choosing date, period and journal ;
-    * Provide an interface to correct account moves with incorrect 'date' / 'period' values ; 
+    * Help accounting people by filling period_id correctly when choosing
+      date, period and journal;
+    * Provide an interface to correct account moves with incorrect 'date'
+      / 'period' values;
 
 Technical informations :
 ------------------------
-    * You can use 'account-financial-tools'/'account_journal_always_check_date' to prevent having new account moves with incorrect 'date' and 'periods' values ;
+    * You can use 'account-financial-tools'/'account_journal_always_check_date'
+      to prevent having new account moves with incorrect 'date' and 'periods'
+      values;
 
-Contacts :
-----------
-    * Sylvain LE GAL (https://twitter.com/legalsylvain); 
-    * <informatique@grap.coop> for any help or question about this module.
+Copyright, Author and Licence :
+-------------------------------
+    * Copyright : 2013, Groupement Régional Alimentaire de Proximité;
+    * Author :
+        * Sylvain LE GAL (https://twitter.com/legalsylvain);
+    * Licence : AGPL-3 (http://www.gnu.org/licenses/)
     """,
     'author': 'GRAP',
     'website': 'http://www.grap.coop',
@@ -29,9 +55,7 @@ Contacts :
     'depends': [
         'account',
     ],
-    'init_xml': [],
-    'demo_xml': [],
-    'update_xml': [
+    'data': [
         'security/res_groups.yml',
         'view/account_move_view.xml',
         'view/view.xml',
@@ -39,7 +63,4 @@ Contacts :
         'view/menu.xml',
         'view/account_period_view.xml',
     ],
-    'application' : True,
-    'installable': True,
-    'auto_install': False,
 }
--- account_move_period_date_conform/model/account_move.py
+++ account_move_period_date_conform/model/account_move.py
@@ -1,27 +1,50 @@
 # -*- encoding: utf-8 -*-
-################################################################################
-#    See __openerp__.py file for Copyright and Licence Informations.
-################################################################################
+##############################################################################
+#
+#    Account Move - Date and Ceriod Conform Module for Odoo
+#    Copyright (C) 2013-Today GRAP (http://www.grap.coop)
+#    @author Sylvain LE GAL (https://twitter.com/legalsylvain)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
 
 from openerp.osv.orm import Model
-from openerp.tools.translate import _
+
 
 class account_move(Model):
     _inherit = 'account.move'
-    
-    def _get_period_from_date_company_id(self, cr, uid, date, company_id, context=None):
-        if not context: 
+
+    def _get_period_from_date_company_id(
+            self, cr, uid, date, company_id, context=None):
+        if not context:
             context = {}
         ctx = context.copy()
-        ctx.update({'company_id': company_id, 'account_period_prefer_normal': True})
-        return self.pool.get('account.period').find(cr, uid, date, context=ctx)[0]
+        ctx.update({
+            'company_id': company_id,
+            'account_period_prefer_normal': True})
+        return self.pool.get('account.period').find(
+            cr, uid, date, context=ctx)[0]
 
-    def onchange_date_company_journal(self, cr, uid, ids, date, company_id, journal_id, context=None):
+    def onchange_date_company_journal(
+            self, cr, uid, ids, date, company_id, journal_id, context=None):
         if not company_id or not date or not journal_id:
             return {}
-        if self.pool.get('account.journal').browse(cr, uid, journal_id, context=context).allow_date:
-            period_id = self._get_period_from_date_company_id(cr, uid, date, company_id, context=context)
+        if self.pool.get('account.journal').browse(
+                cr, uid, journal_id, context=context).allow_date:
+            period_id = self._get_period_from_date_company_id(
+                cr, uid, date, company_id, context=context)
             return {'value': {'period_id': period_id}}
-        else: 
+        else:
             return {}
-
