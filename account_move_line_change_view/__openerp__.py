5900aaf9496b8d95fd6ad8a7bedd174ac6197d0d
Sylvain LE GAL <sylvain.legal@grap.coop>
2014-11-07 19:23:36 +0100

    [REF] 'account_move_line_change_view' flake8

--- account_move_line_change_view/__openerp__.py
+++ account_move_line_change_view/__openerp__.py
@@ -1,4 +1,24 @@
-# -*- coding: utf-8 -*-
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Account - Move line Change view Module for Odoo
+#    Copyright (C) 2013-Today GRAP (http://www.grap.coop)
+#    @author Sylvain LE GAL (https://twitter.com/legalsylvain)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
 {
     'name': 'Account - Move line Change view',
     'version': '1.0',
@@ -7,30 +27,25 @@
 Replace the weird tree view by a form view for account move line
 ================================================================
 
-Contacts :
-----------
-    * Sylvain LE GAL (https://twitter.com/legalsylvain);
-    * <informatique@grap.coop> for any help or question about this module.
+TODO:
+-----
+    * Refactor into grap_change_views;
+
+Copyright, Author and Licence :
+-------------------------------
+    * Copyright : 2014, Groupement Régional Alimentaire de Proximité;
+    * Author :
+        * Sylvain LE GAL (https://twitter.com/legalsylvain);
+    * Licence : AGPL-3 (http://www.gnu.org/licenses/)
     """,
     'author': 'GRAP',
     'website': 'http://www.grap.coop',
-    'license': 'AGPL-3',
     'depends': [
             'account',
-        ],
+    ],
     'data': [
         'view/view.xml',
         'view/action.xml',
         'view/menu.xml',
-        ],
-    'demo': [],
-    'js': [],
-    'css': [],
-    'qweb': [],
-    'images': [],
-    'post_load': '',
-    'application': False,
-    'installable': True,
-    'auto_install': False,
-    'images': [],
+    ],
 }
