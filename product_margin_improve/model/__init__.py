e92c8f413fd3c75ebf5f543cbbf462b3aa946479
Sylvain LE GAL <sylvain.legal@grap.coop>
2014-11-08 01:25:07 +0100

    [REF] various modules flake8 / pyling;

--- product_margin_improve/model/__init__.py
+++ product_margin_improve/model/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
-#    
+#
 #    OpenERP, Open Source Management Solution
 #    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
 #
