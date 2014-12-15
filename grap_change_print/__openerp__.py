646743760a1e961621da0d399124636eb4ea68b8
Sylvain LE GAL <sylvain.legal@grap.coop>
2014-12-15 15:56:08 +0100

    [REF] merge 'pos_statement_report' into 'grap_change_print';

--- grap_change_print/__openerp__.py
+++ grap_change_print/__openerp__.py
@@ -3,6 +3,7 @@
 #
 #    GRAP - Change Print module for Odoo
 #    Copyright (C) 2013-2014 GRAP (http://www.grap.coop)
+#    @author Julien WESTE
 #    @author Sylvain LE GAL (https://twitter.com/legalsylvain)
 #
 #    This program is free software: you can redistribute it and/or modify
@@ -28,12 +29,17 @@
 Change default reporting
 ========================
     * account.invoice:
-        *
+        * Add extra mandatory information of each line about food;
+        * Remove Unit Price column and set an Unit Price VAT Exclude column;
+    * account.bank.statement:
+        * Add the number of lines at the end of the report;
 
 Copyright, Author and Licence :
 -------------------------------
     * Copyright : 2014, Groupement Régional Alimentaire de Proximité;
-    * Author : Sylvain LE GAL (https://twitter.com/legalsylvain);
+    * Author :
+        * Julien WESTE;
+        * Sylvain LE GAL (https://twitter.com/legalsylvain);
     * Licence : AGPL-3 (http://www.gnu.org/licenses/)
     """,
     'author': 'GRAP',
--- pos_statement_report/report/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- encoding: utf-8 -*-
-###############################################################################
-#    See __openerp__.py file for Copyright and Licence Informations.
-###############################################################################
-
-from . import account_statement
