5b5aa21e6898ac660ca2c0fef8382628be9dbc08
Sylvain LE GAL <sylvain.legal@grap.coop>
2015-01-19 15:24:25 +0100

    [REF] 'stock_inventory_stock_duplicates' : add tests and descriptions;

--- stock_inventory_sum_duplicates/__openerp__.py
+++ stock_inventory_sum_duplicates/__openerp__.py
@@ -4,6 +4,7 @@
 #    Stock Inventory - Sum Duplicates for Odoo
 #    Copyright (C) 2013-Today GRAP (http://www.grap.coop)
 #    @author Julien WESTE
+#    @author Sylvain LE GAL (https://twitter.com/legalsylvain)
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -25,15 +26,25 @@
     'version': '1.1',
     'category': 'Stock',
     'description': """
-If a stock.inventory has several lines with same product, lot and location,
-the lines will be merged and the quantities will be summed before creating
-the stock.moves
+Feature:
+--------
+    * If a stock.inventory has several lines with same product, lot and
+      location, the lines will be merged and the quantities will be summed
+      before creating the stock.moves;
+
+Use Case:
+---------
+    * This module is usefull when the product is in many location that are
+      not defined in OpenERP. For example, in a little shop, a product can be:
+        * in stock; (in one or many places);
+        * in the sales area; (in one or many places);
 
 Copyright, Author and Licence :
 -------------------------------
     * Copyright : 2013, Groupement Régional Alimentaire de Proximité;
     * Author :
         * Julien WESTE;
+        * Sylvain LE GAL (https://twitter.com/legalsylvain);
     * Licence : AGPL-3 (http://www.gnu.org/licenses/)
     """,
     'author': 'GRAP',
@@ -41,5 +52,9 @@ Copyright, Author and Licence :
     'license': 'AGPL-3',
     'depends': [
         'stock',
+    ],
+    'demo': [
+        'demo/res_groups.yml',
+        'demo/stock_inventory.yml',
     ],
 }
---
+++ stock_inventory_sum_duplicates/tests/tests.py
@@ -0,0 +1,87 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Stock Inventory - Sum Duplicates for Odoo
+#    Copyright (C) 2014-Today GRAP (http://www.grap.coop)
+#    @author Sylvain LE GAL (https://twitter.com/legalsylvain)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp.tests.common import TransactionCase
+
+
+class TestAccountGroupMoveLines(TransactionCase):
+
+    def setUp(self):
+        super(TestAccountGroupMoveLines, self).setUp()
+        cr, uid = self.cr, self.uid
+        self.imd_obj = self.registry('ir.model.data')
+        self.si_obj = self.registry('stock.inventory')
+        self.stock_inventory_id = self.imd_obj.get_object_reference(
+            cr, uid, 'stock_inventory_sum_duplicates',
+            'stock_inventory_sum_duplicates')[1]
+        self.stock_location_1_id = self.imd_obj.get_object_reference(
+            cr, uid, 'stock',
+            'stock_location_components')[1]
+        self.stock_location_2_id = self.imd_obj.get_object_reference(
+            cr, uid, 'stock',
+            'stock_location_14')[1]
+        self.product_1_id = self.imd_obj.get_object_reference(
+            cr, uid, 'product',
+            'product_product_48')[1]
+        self.product_2_id = self.imd_obj.get_object_reference(
+            cr, uid, 'product',
+            'product_product_24')[1]
+
+    # Test Section
+    def test_01_confirm_stock_inventory(self):
+        """[Functional Test] Confirm Stock Inventory with many references to
+        the same products"""
+        cr, uid = self.cr, self.uid
+        self.si_obj.action_confirm(cr, uid, [self.stock_inventory_id])
+
+        si = self.si_obj.browse(cr, uid, self.stock_inventory_id)
+
+        self.assertEquals(
+            len(si.inventory_line_id), 3,
+            """Merge Failed : Incorrect Number of lines.""")
+
+        # Test if merge is realized or not
+        lineNumber = 0
+        for line in si.inventory_line_id:
+            if (
+                    line.location_id.id == self.stock_location_1_id and
+                    line.product_id.id == self.product_1_id):
+                lineNumber += 1
+                self.assertEquals(
+                    line.product_qty, 31,
+                    """Merge Failed: Incorrect Quantity after merge.""")
+            elif (
+                    line.location_id.id == self.stock_location_1_id and
+                    line.product_id.id == self.product_2_id):
+                self.assertEquals(
+                    line.product_qty, 32,
+                    """Quantity changed, behaviour not expected.""")
+            elif (
+                    line.location_id.id == self.stock_location_2_id and
+                    line.product_id.id == self.product_1_id):
+                self.assertEquals(
+                    line.product_qty, 43,
+                    """Quantity changed, behaviour not expected.""")
+            else:
+                self.assertEquals(
+                    True, False,
+                    """Unexpected Line""")
