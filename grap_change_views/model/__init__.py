6a826de87e47ad3abdd1829225697f551ea4b30c
Sylvain LE GAL <sylvain.legal@grap.coop>
2015-06-17 18:56:30 +0200

    [REF] grap_change_view : move stock_move.sequence into odoo-addons-misc / stock_move_add_seq;

--- grap_change_views/model/__init__.py
+++ grap_change_views/model/__init__.py
@@ -22,7 +22,6 @@
 ##############################################################################
 
 from . import account_invoice_line
-from . import stock_move
 from . import pos_category
 from . import pos_order_line
 from . import product_product
