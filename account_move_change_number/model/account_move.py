40f387743df5921cc03651d587fc2c9b160ad977
Sylvain LE GAL <sylvain.legal@grap.coop>
2015-12-03 16:45:35 +0100

    [FIX] xml file not included in openerp file; [REF] 'move change number' improve description ;

--- account_move_change_number/model/account_move.py
+++ account_move_change_number/model/account_move.py
@@ -20,6 +20,8 @@
 #
 ##############################################################################
 
+from datetime import datetime
+
 from openerp.osv.orm import Model
 from openerp.tools.translate import _
 
@@ -28,24 +30,29 @@ class account_move(Model):
     _inherit = 'account.move'
 
     def rename_account_move_change_number(self, cr, uid, ids, context=None):
-        for aId in ids:
-            old_account_move = self.browse(cr, uid, aId, context=context)
+        user_obj = self.pool['res.users']
+        for id in ids:
+            old_account_move = self.browse(cr, uid, id, context=context)
             old_name = old_account_move.name
             old_narration = old_account_move.narration or ''
+
             # unpost acount move
-            self.button_cancel(cr, uid, [aId], context=context)
+            self.button_cancel(cr, uid, [id], context=context)
 
             # set name to "/"
-            self.write(cr, uid, [aId], {'name': '/'}, context=context)
+            self.write(cr, uid, [id], {'name': '/'}, context=context)
 
             # post account move
-            self.post(cr, uid, [aId], context=context)
-            new_name = self.browse(cr, uid, aId, context=context).name
+            self.post(cr, uid, [id], context=context)
+            new_name = self.browse(cr, uid, id, context=context).name
 
             # Add description of the change
-            self.write(cr, uid, [aId], {
+            date = datetime.today().strftime("%d/%m/%Y")
+            author = user_obj.browse(cr, uid, uid, context=context).name
+            self.write(cr, uid, [id], {
                 'narration': old_narration + _(
-                    """\nAccount move renamed old name : %s ;"""
-                    """ new name : %s""") % (old_name, new_name)
+                    "\nAccount move renamed. Old name : %s."
+                    " New name : %s. Rename date : %s. Author : %s.") % (
+                        old_name, new_name, date, author)
             }, context=context)
         return True
