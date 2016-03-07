b3e36f21a5bc9e068ce7fd7a820d1af397b3fe37
Sylvain LE GAL <sylvain.legal@grap.coop>
2016-03-07 23:06:52 +0100

    [ADD] grap_print_product;

--- grap_print_product/__openerp__.py
+++ grap_print_product/__openerp__.py
@@ -1,24 +1,7 @@
-# -*- encoding: utf-8 -*-
-##############################################################################
-#
-#    GRAP - Print Product module for Odoo
-#    Copyright (C) 2015-Today GRAP (http://www.grap.coop)
-#    @author Sylvain LE GAL (https://twitter.com/legalsylvain)
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# -*- coding: utf-8 -*-
+# Copyright (C) 2016-Today: GRAP (<http://www.grap.coop>)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'GRAP - Print Product',
@@ -54,12 +37,13 @@ Limits / Roadmap
   merge odoo-addons-misc/grap_print_product
   and odoo-addons-grap/grap_change_print
 
-Copyright, Authors and Licence:
--------------------------------
-    * Copyright: 2015, GRAP: Groupement Régional Alimentaire de Proximité;
-    * Author:
-        * Sylvain LE GAL (https://twitter.com/legalsylvain);
-    * Licence: AGPL-3 (http://www.gnu.org/licenses/);""",
+Copyright, Authors and Licence
+------------------------------
+
+* Copyright: 2015, GRAP: Groupement Régional Alimentaire de Proximité;
+* Author:
+    * Sylvain LE GAL (https://twitter.com/legalsylvain);
+* Licence: AGPL-3 (http://www.gnu.org/licenses/);""",
     'author': 'GRAP',
     'website': 'http://www.grap.coop',
     'license': 'AGPL-3',
@@ -68,11 +52,18 @@ Copyright, Authors and Licence:
         'report_webkit',
     ],
     'data': [
+        'security/res_groups.yml',
         'security/ir_model_access.yml',
+        'security/ir_rule.xml',
         'report/print_product_1_report.xml',
-        'views/view.xml',
+        'views/print_product_type_view.xml',
+        'views/print_product_wizard_view.xml',
+        'views/product_product_view.xml',
         'views/action.xml',
         'views/menu.xml',
+    ],
+    'demo': [
+        'demo/res_groups.yml',
     ],
     'external_dependencies': {
         'python': ['cairosvg'],
