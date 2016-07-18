89dbff0682f538c645c4d7fb60395af13b797f89
Sylvain LE GAL <sylvain.legal@grap.coop>
2016-07-18 21:12:36 +0200

    [ADD] new view for account.invoice.line;

--- grap_change_views/model/account_invoice_line.py
+++ grap_change_views/model/account_invoice_line.py
@@ -7,11 +7,59 @@ from openerp.osv import fields
 class AccountInvoiceLine(Model):
     _inherit = 'account.invoice.line'
 
+    # Invalidation Section
+    def _get_invoice_line_by_invoice(self, cr, uid, ids, context=None):
+        """ids are account.invoice"""
+        return self.pool['account.invoice.line'].search(
+            cr, uid, [('invoice_id', 'in', ids)], context=context)
+
+    def _get_invoice_line_by_product(self, cr, uid, ids, context=None):
+        """ids are product.product"""
+        return self.pool['account.invoice.line'].search(
+            cr, uid, [('product_id', 'in', ids)], context=context)
+
     # Column Section
     _columns = {
         'tax_ids_readonly': fields.related(
             'invoice_line_tax_id', type='many2many', relation='account.tax',
             readonly=True, string='Taxes'),
+
+        # Invoice related
+        'type': fields.related(
+            'invoice_id', 'type', string='Type', type='char', select=True,
+            store={
+                'account.invoice': (
+                    _get_invoice_line_by_invoice, ['type'], 10),
+            }),
+        'state': fields.related(
+            'invoice_id', 'state', string='Type', type='char', select=True,
+            store={
+                'account.invoice': (
+                    _get_invoice_line_by_invoice, ['state'], 10),
+            }),
+        'partner_id': fields.related(
+            'invoice_id', 'partner_id', string='Partner',
+            relation='res.partner', type='many2one', select=True, store={
+                'account.invoice': (
+                    _get_invoice_line_by_invoice, ['period_id'], 10),
+            }),
+        'date_invoice': fields.related(
+            'invoice_id', 'date_invoice', string='Date invoice', type='date',
+            select=True, store={
+                'account.invoice': (
+                    _get_invoice_line_by_invoice, ['date_invoice'], 10),
+            }),
+
+        # product related
+        'categ_id': fields.related(
+            'product_id', 'categ_id', string='Category', select=True,
+            relation='product.category', type='many2one', store={
+                'account.invoice.line': (
+                    lambda self, cr, uid, ids, c=None: ids, ['product_id'],
+                    10),
+                'product.product': (
+                    _get_invoice_line_by_product, ['categ_id'], 10),
+            }),
     }
 
     def product_id_change(
