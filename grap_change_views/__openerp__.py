393d4e4400870e08a1399fd0510fa2def9319be0
Sylvain LE GAL <sylvain.legal@grap.coop>
2017-08-10 14:26:41 +0200

    add migration script grap_change_views

--- grap_change_views/__openerp__.py
+++ grap_change_views/__openerp__.py
@@ -1,29 +1,11 @@
-# -*- encoding: utf-8 -*-
-##############################################################################
-#
-#    GRAP - Change Views module for Odoo
-#    Copyright (C) 2013 GRAP (http://www.grap.coop)
-#    @author Julien WESTE
-#    @author Sylvain LE GAL (https://twitter.com/legalsylvain)
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-
+# -*- coding: utf-8 -*-
+# Copyright (C) 2013-Today: GRAP (http://www.grap.coop)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# @author Julien WESTE
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'GRAP - Change Views',
-    'version': '6.0',
+    'version': '7.0',
     'category': 'GRAP - Custom',
     'description': """
 Show / Hide / change views for users
@@ -34,6 +16,8 @@ Functionality:
     * Hide a lot of field from 'basic users';
     * Change size of some columns;
     * Add product's function field in pos_category;
+
+TODO: update description
 
 Copyright, Authors and Licence:
 -------------------------------
