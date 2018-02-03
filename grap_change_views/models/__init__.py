35049fe097df907f79303c000c637e7946463c78
Sylvain LE GAL <sylvain.legal@grap.coop>
2018-02-03 19:03:42 +0100

    [FIX] stock picking display

--- grap_change_views/models/__init__.py
+++ grap_change_views/models/__init__.py
@@ -6,6 +6,7 @@ from . import pos_order_line
 from . import product_product
 from . import purchase_order_line
 from . import sale_order_line
+from . import stock_picking
 from . import stock_inventory
 from . import stock_inventory_line
 from . import stock_move
