0c90270aa799165949b33368817868a2b033c4a1
Sylvain LE GAL <sylvain.legal@grap.coop>
2018-02-05 21:15:19 +0100

    [WIP] add grap_qweb_report

---
+++ grap_qweb_report/README.rst
@@ -0,0 +1,22 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+   :target: https://www.gnu.org/licenses/agpl
+   :alt: License: AGPL-3
+
+===================
+GRAP - QWeb Reports
+===================
+
+Redefine default odoo qweb reports for GRAP.
+
+Credits
+=======
+
+Contributors
+------------
+
+* Sylvain LE GAL <https://twitter.com/legalsylvain>
+
+Funders
+-------
+
+* GRAP, Groupement Régional Alimentaire de Proximité <http://www.grap.coop>
---
+++ grap_qweb_report/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'GRAP - Custom Qweb Reports',
+    'version': '8.0.1.0.0',
+    'category': 'GRAP - Custom',
+    'author': 'GRAP',
+    'website': 'http://www.grap.coop',
+    'license': 'AGPL-3',
+    'depends': [
+        'sale',
+        'report', # TMP
+        'report_webkit',
+    ],
+    'data': [
+        'report/qweb_template_layout.xml',
+        'report/qweb_template_sale_order.xml',
+        'report/ir_actions_report_xml.xml',
+    ],
+}
