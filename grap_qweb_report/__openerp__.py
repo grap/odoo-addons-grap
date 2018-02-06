1fc00583a3e9032c77b4353feff7c1a911d4081d
Sylvain LE GAL <sylvain.legal@grap.coop>
2018-02-06 16:58:38 +0100

    [WIP] purchase report

--- grap_qweb_report/__openerp__.py
+++ grap_qweb_report/__openerp__.py
@@ -7,14 +7,16 @@
     'website': 'http://www.grap.coop',
     'license': 'AGPL-3',
     'depends': [
+        'purchase',
         'sale',
-        'report', # TMP
-        'report_webkit',
+        'l10n_fr_siret',
         'base_fiscal_company',
     ],
     'data': [
         'report/qweb_template_layout.xml',
         'report/qweb_template_sale_order.xml',
+        'report/qweb_template_purchase_order.xml',
+        'report/qweb_template_purchase_order_quotation.xml',
         'report/ir_actions_report_xml.xml',
     ],
 }
