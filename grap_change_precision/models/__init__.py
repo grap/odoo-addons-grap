e6011bf32e0c7ffb219565321736ba4c9cbebc78
Sylvain LE GAL <sylvain.legal@grap.coop>
2018-07-13 12:25:08 +0200

    [REF] move into grap-odoo-custom

--- grap_change_precision/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# coding: utf-8
-
-from . import account_invoice_line
-from . import pricelist_partnerinfo
-from . import product_template
-from . import pos_order_line
-from . import purchase_order_line
