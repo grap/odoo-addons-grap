954a3b37af5c3993e64437fd6075a7d404454645
Sylvain LE GAL <sylvain.legal@grap.coop>
2018-07-30 18:04:25 +0200

    [REF] move custom display of lines from stock_inventory_merge

--- grap_change_views/models/stock_inventory_line.py
+++ grap_change_views/models/stock_inventory_line.py
@@ -3,9 +3,14 @@
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp.osv.orm import Model
+from openerp import fields, models
 
 
-class StockInventoryLine(Model):
+class StockInventoryLine(models.Model):
     _inherit = 'stock.inventory.line'
     _order = 'inventory_id, id'
+
+    product_categ_id = fields.Many2one(
+        comodel_name='product.category', string='Product Category',
+        readonly=True,
+        related='product_id.categ_id', store=True)
